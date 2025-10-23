7eb10dfc86412f9d6cae4e5e1a5816e7e0b7ff46
cyberxkomban <komban62934@gmail.com>
2025-10-23 16:53:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,18 @@
 /*
 
-|       \         |  \  /  \
-| ▓▓▓▓▓▓▓\ ______ | ▓▓ /  ▓▓
-| ▓▓__/ ▓▓/      \| ▓▓/  ▓▓
-| ▓▓    ▓▓  ▓▓▓▓▓▓\ ▓▓  ▓▓
-| ▓▓▓▓▓▓▓\ ▓▓   \▓▓ ▓▓▓▓▓\
-| ▓▓__/ ▓▓ ▓▓     | ▓▓ \▓▓\
-| ▓▓    ▓▓ ▓▓     | ▓▓  \▓▓\
- \▓▓▓▓▓▓▓ \▓▓      \▓▓   \▓▓         
+|
+░█████╗░██╗░░░██╗██████╗░███████╗██████╗░
+██╔══██╗╚██╗░██╔╝██╔══██╗██╔════╝██╔══██╗
+██║░░╚═╝░╚████╔╝░██████╦╝█████╗░░██████╔╝
+██║░░██╗░░╚██╔╝░░██╔══██╗██╔══╝░░██╔══██╗
+╚█████╔╝░░░██║░░░██████╦╝███████╗██║░░██║
+░╚════╝░░░░╚═╝░░░╚═════╝░╚══════╝╚═╝░░╚═╝
 
 
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   # MADE BY CYBER!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/R3yJFjceCD ]
+
 */
 
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
